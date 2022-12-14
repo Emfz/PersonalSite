2eb60e1cbc9298a830e26bd354a6f1b15d90566e
Emfz <enrique.mfz@outlook.com>
2022-12-14 17:25:00 -0600

    Add base,index,navbar,styles,btstrp. Remove flask-btstrap

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
+from flask import Flask, render_template, url_for
+
 
 app = Flask(__name__)
-Bootstrap(app)
 
 @app.route("/")
 def home():
