cd6336ca335eed3515feb7a0053291b5a292dd12
Emfz <enrique.mfz@outlook.com>
2022-12-20 13:55:44 -0600

    First version of the site, including structure and style.

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, url_for
+import time
 
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return render_template("index.html")
+	year = time.localtime().tm_year
+	return render_template("index.html", year = year)
+
+
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run(debug=True, host="0.0.0.0")
