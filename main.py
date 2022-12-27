276c0c5dab4c842fe06beca45c1c6775c4a40f52
Emfz <enrique.mfz@outlook.com>
2022-12-27 15:51:46 -0600

    First version of cv complete

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ import time
 
 
 app = Flask(__name__)
+year = time.localtime().tm_year
 
 @app.route("/")
 def home():
-	year = time.localtime().tm_year
 	return render_template("index.html", year = year)
+
+@app.route("/cv")
+def cv():
+	return render_template("cv.html", year = year)
 
 
 
 if __name__ == "__main__":
 	app.run(debug=True, host="0.0.0.0")
+	# app.run(debug=True)
